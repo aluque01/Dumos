237ec48c327d14a8caa63c9d16491aa6f1338600
Obaid Farooqui <ofarooqui12crh@gmail.com>
2016-04-07 11:43:14 -0400

    Moved everything to DumosApp folder

---
+++ DumosApp/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  
+//
+//  Created by Ariel Luque on 3/6/16.
+//
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <PTDBeanManager.h>
+
+
+#endif /* Bridging_Header_h */
